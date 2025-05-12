003774ac3613732aa3ae0f2d2001dd8eaf0ef3b8
simonkotslay <164027164+simonkotslay@users.noreply.github.com>
2025-05-12 03:01:09 -0600

    header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Home from './pages/Home/Home'
 function App() {
 
   return (
     <>
       <div className='App'>
-        <h1>Hello</h1>
+        <Home/>
       </div>
     </>
   )
